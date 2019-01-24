351b2ee00b204fcdf01c8897d8344be7582faa17
Ryan McCaul <Ryan.McCaul@tmx.com>
2019-01-24 16:51:08 -0500

    tttttttt

--- main.tf
+++ main.tf
@@ -21,8 +21,7 @@ data "template_file" "vault_init" {
 
 module "vault_server_sg" {
   source  = "terra.tmx.cloud/tmx-cloud/module-vault-server-ports-aws/aws"
-  version = "0.1.3"
-
+  version = "v0.1.4"
   create      = "${var.create ? 1 : 0}"
   name        = "${var.name}-vault-server"
   vpc_id      = "${var.vpc_id}"
